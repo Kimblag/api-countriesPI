369e94dd854636380693bdef1f0fc2e41f19ebde
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-04-27 18:11:49 -0300

    added edit and delete routes

--- src/controllers/activity.controller.js
+++ src/controllers/activity.controller.js
@@ -4,6 +4,16 @@ const axios = require("axios");
 const { Country, Activity } = require("../db.js");
 const { Op } = require("sequelize");
 
+const getActivity = (req, res, next) => {
+  Activity.findAll()
+    .then((activity) => {
+      res.status(200).send(activity);
+    })
+    .catch((error) => {
+      res.status(500).send({ message: "Error getting activity" });
+    });
+}
+
 const createActivity = async (req, res, next) => {
   try {
     const { countries, name, difficulty, duration, season } = req.body;
@@ -28,6 +38,72 @@ const createActivity = async (req, res, next) => {
   }
 };
 
+const updateActivity = (req, res, next) => {
+  const { id } = req.params;
+  console.log("Soy el id", id)
+  const { name, difficulty, duration, season } = req.body;
+  parseInt(difficulty);
+  parseInt(duration);
+  name.toLowerCase();
+  const activityDetail = Activity.findByPk(id)
+  console.log(difficulty, "de BACKEND")
+  
+  activityDetail.then(activity =>{
+    activity.update({
+      name,
+      difficulty,
+      duration,
+      season,
+    })
+  }).then(() => {
+    res.status(200).send({message: "Activity updated successfully"});
+  }).catch(error => {
+    res.status(500).send({ message: "Error updating activity", error });
+  })
+};
+
+const getActivityDetails = (req, res, next) => {
+  let { id } = req.params;
+  id = parseInt(id);
+    if (id) {
+      let activity = Activity.findByPk(id);
+      activity
+        .then((response) => {
+          if (!response) {
+            return res.status(404).send({ message: "Activity not found" });
+          } else {
+            return res.status(200).send(response);
+          }
+        })
+        .catch((error) =>
+          res.send({ message: "Error getting activity", error })
+        );
+    } else {
+    res.status(400).send({ message: "Invalid country id" });
+  }
+};
+
+
+
+const deleteActivity = (req, res, next) => {
+  const { id } = req.params;
+  Activity.destroy({
+    where: {
+      id,
+    },
+  })
+    .then((activity) => {
+      res.status(200).send({ message: "Activity deleted successfully" });
+    })
+    .catch((error) => {
+      res.status(500).send({ message: "Error deleting activity", error });
+    });
+};
+
 module.exports = {
   createActivity,
+  updateActivity,
+  deleteActivity,
+  getActivity,
+  getActivityDetails
 };
--- src/routes/Activities.js
+++ src/routes/Activities.js
@@ -1,7 +1,11 @@
 var express = require("express");
-const { createActivity } = require("../controllers/activity.controller");
+const { createActivity, updateActivity, deleteActivity, getActivity, getActivityDetails} = require("../controllers/activity.controller");
 var router = express.Router();
 
+router.get("/activities", getActivity);
+router.get("/activities/:id", getActivityDetails);
 router.post("/", createActivity);
+router.put("/:id", updateActivity);
+router.delete("/:id", deleteActivity);
 
 module.exports = router;
